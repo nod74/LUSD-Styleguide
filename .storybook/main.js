b5eb0b9a365a59ab15c04178bb3453dfc5da14c8
nod74 <s-l-@gmx.de>
2022-07-15 12:56:37 +0200

    1
    1

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,8 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-      "@storybook-zeplin/register",
+    "@storybook-zeplin/register",
+      "@storybook/addon-a11y",
   ],
   framework: "@storybook/angular",
   core: {
